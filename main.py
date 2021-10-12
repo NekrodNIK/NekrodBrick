dcb30b4d4c4914bc6917a646ee6915a0143715df
NekrodNIK <60639354+NekrodNIK@users.noreply.github.com>
2021-10-12 20:56:35 +0500

    add black/white filter and contrast filter

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
-from PIL import Image
+from PIL import Image, ImageEnhance
 
 img = Image.open("./example_image/road.jpg")
 
 imgSmall = img.resize((64, 64), resample=Image.BILINEAR)
 
 result = imgSmall.resize(img.size, resample=Image.NEAREST)
+
+result = ImageEnhance.Contrast(result).enhance(1.5)
+
+result = result.convert("L")
 
 result.save("./example_image/road-pixelize.jpg")
